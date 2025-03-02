17f93c91e4a22e50633e99044b72b76be20fe4be
sameertantry <sameertantry@gmail.com>
2025-03-02 18:09:47 +0300

    fixed bugs

--- train_sae.py
+++ train_sae.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 import hydra
 from torch.utils.tensorboard import SummaryWriter
 from dust3r.model import AsymmetricCroCo3DStereo, inf  # noqa: F401, needed when loading the model
-from dust3r.datasets import get_data_loader, build_dataset  # noqa
+from dust3r.datasets import get_data_loader  # noqa
 
+from tqdm import tqdm
 from sae import SAE, USAE
 
 def set_all_seeds(seed): 
@@ -14,16 +15,11 @@ def set_all_seeds(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-def load_duts3r(duts3r_ckpt_path, device): 
-    duts3r = AsymmetricCroCo3DStereo(patch_embed_cls='ManyAR_PatchEmbed')
-    ckpt = torch.load(duts3r_ckpt_path, map_location=device)
-    duts3r.load_state_dict(ckpt['model'], strict=False))
-    del ckpt
-    return duts3r
+def load_dust3r(dust3r_ckpt_path, device): 
+    return AsymmetricCroCo3DStereo.from_pretrained(dust3r_ckpt_path, device=device)
     
 @hydra.main(version_base=None, config_path="configs", config_name="main")    
 def train_sae(cfg): 
-    
     
     dataloader = get_data_loader(
 	cfg.train_dataset,
@@ -36,37 +32,39 @@ def train_sae(cfg):
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     
-    duts3r = load_duts3r(cfg.duts3r_ckpt_path)
-    duts3r = duts3r.to(device)    
-
-    # ToDo : Choose hidden_dim based on selected layer
-    sae = SAE(hidden_dim, int(cfg.sparsity_coeff*hidden_dim) )
-    optimizer = torch.nn.optim.Adam(sae.parameters())
-    activation = {}
+    dust3r = load_dust3r(cfg.dust3r_ckpt_path, device)
+    dust3r = dust3r.to(device) 
+   
+    hidden_dim = dust3r.enc_norm.normalized_shape[0]
     
-    def getActivation(name):
-        def hook(model, input, output):
-            activation[name] = output.detach()
-        return hook
+    sae = SAE(hidden_dim, int(hidden_dim // cfg.sparsity_coeff))
+    optimizer = torch.optim.Adam(sae.parameters())
 
     # ToDo : Choose a layer to apply SAE for 
-    hook = duts3r.avgpool.register_forward_hook(getActivation('activation'))
     writer = SummaryWriter(cfg.log_path)
     recon_loss = nn.MSELoss()
 
-    for epoch_idx in range(n_epochs): 
-        for batch_idx, batch in enumerate(dataloader): 
+    for epoch_idx in range(cfg.num_epochs): 
+        for batch_idx, batch in tqdm(enumerate(dataloader), total=len(dataloader)): 
             
             glob_iter = batch_idx + epoch_idx*len(dataloader)
+            ignore_keys = set(['depthmap', 'dataset', 'label', 'instance', 'idx', 'true_shape', 'rng'])
+            for view in batch:
+                for name in view.keys():  # pseudo_focal
+                    if name in ignore_keys:
+                        continue
+                    view[name] = view[name].to(device, non_blocking=True)
             with torch.inference_mode(): 
-              _ = duts3r(X)
-            act = activation['activation']
-            
+                view1, view2 = batch
+                (shape1, shape2), (feat1, feat2), (pos1, pos2) = dust3r._encode_symmetrized(view1, view2)
+            print("infered") 
+            act = torch.concat((feat1, feat2), dim=1)
             optimizer.zero_grad()
-            act_recon, act_encode = sae(act)
+            act_encode, act_recon = sae(act)
             loss = recon_loss(act, act_recon) + cfg.reg_coeff * act_encode.abs().sum()
             loss.backward() 
             optimizer.step()
+            print("step")
 
             writer.add_scalar("Train/loss_iter", loss.item(),  glob_iter)
             writer.add_scalar("Train/epoch_iter", epoch_idx + 1, glob_iter)
